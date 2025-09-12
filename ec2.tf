85ea653fdcae9c5c282aa5db9915ddc4494b95c2
The-Kadabra <gabriel.pires.ti@gmail.com>
2025-09-12 08:43:30 -0300

    add:ec2

--- ec2.tf
+++ ec2.tf
@@ -1,25 +1,35 @@
 #################
 #Virtual Machine#
 #################
-/*module "ec2_instance" {
+module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-
-  name = "spot-instance"
+  depends_on = [module.vpc]
+  name = "spot-instance-desafio-warren"
 
   create_spot_instance = true
   spot_price           = "0.60"
   spot_type            = "persistent"
 
-  instance_type = "t3.micro"
-  key_name      = "user1"
-  monitoring    = true
-  subnet_id     = "subnet-eddcdzz4"
+  instance_type         = "t3.micro"
+  key_name              = "user1"
+  monitoring            = true
+  subnet_id             = module.vpc.private_subnets[0]
+  
+  create_security_group  = true
+  security_group_name    = format("sg-%s-%s",var.project, var.environment)
+  security_group_description = format("%s-%s",var.project, var.environment)
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
+
+security_group_egress_rules = {
+  all_egress = {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound"
+    }
   }
-}*/
+}
 
 #####
 #VPC#
